6c94020e4955cde81b008307280cc866659ac716
SHiN201007 <shin1007m@gmail.com>
2019-09-27 12:07:41 +0900

    first push

---
+++ what'sNum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  what'sNum
+//
+//  Created by 松丸真 on 2019/09/27.
+//  Copyright © 2019 松丸真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
