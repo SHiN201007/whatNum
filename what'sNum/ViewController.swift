021071bd2b44bda78d928ae0e3b0b1e36a202f27
reo03ARMS <reo03masu@gmail.com>
2019-09-27 17:16:25 +0900

    aaa

--- what'sNum/ViewController.swift
+++ what'sNum/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    @IBOutlet weak var resultText: UILabel!
+    
+    @IBOutlet weak var userNumber: UITextField!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    override func viewDidLoad(){
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    resultText.text = ""
+    button.backgroundColor = UIColor.blue
+    button.layer.borderWidth = 2.0
+    button.layer.borderColor = UIColor.red.cgColor
+    button.layer.cornerRadius = 10.0
+    button.setTitleColor(UIColor.white,for:.normal)
+    
+    
+        
   }
 
 
